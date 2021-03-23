6ae5f53f68a6731d6f24dabc10736e56f618e7ff
HippieStereo <stereobroo@yahoo.com>
2021-03-23 09:34:21 +0000

    Create JsonSerializationException.java

---
+++ src/main/java/com/hs/annotations_custom_01/JsonSerializationException.java
@@ -0,0 +1,13 @@
+package com.hs.annotations_custom_01;
+
+public class JsonSerializationException extends RuntimeException {
+
+	private static final long serialVersionUID = -8251297270437606705L;
+	
+	public JsonSerializationException(String message) {
+		
+		super(message);
+		
+	}
+
+}
