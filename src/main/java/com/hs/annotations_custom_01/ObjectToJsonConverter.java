786111d7e8653ba80b9f51f9233a0a060ba9470f
HippieStereo <stereobroo@yahoo.com>
2021-03-23 09:39:19 +0000

    Update ObjectToJsonConverter.java
    - Check whether our object is null or not.
    - If its type has the @JsonSerializable annotation.

--- src/main/java/com/hs/annotations_custom_01/ObjectToJsonConverter.java
+++ src/main/java/com/hs/annotations_custom_01/ObjectToJsonConverter.java
@@ -1,5 +1,26 @@
 package com.hs.annotations_custom_01;
 
+import java.util.Objects;
+
 public class ObjectToJsonConverter {
+	
+	private void checkIfSerializable(Object object) {
+		
+		if(Objects.isNull(object)) {
+			
+			throw new JsonSerializationException("The object to serialize is null");
+			
+		}
+		
+		Class<?> clazz = object.getClass();
+		
+		if(!clazz.isAnnotationPresent(JsonSerializable.class)) {
+			
+			throw new JsonSerializationException("The class " 
+					+ clazz.getSimpleName() + " is not annotated with JsonSerializable");
+			
+		}
+		
+	}
 
 }
