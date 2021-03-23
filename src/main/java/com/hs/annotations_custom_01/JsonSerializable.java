ea7ca1f45680f03d0e2a3c9a74d7cac0889ce41d
HippieStereo <stereobroo@yahoo.com>
2021-03-23 09:26:32 +0000

    Class Level
    To indicate to the compiler that our object can be serialized.

---
+++ src/main/java/com/hs/annotations_custom_01/JsonSerializable.java
@@ -0,0 +1,12 @@
+package com.hs.annotations_custom_01;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface JsonSerializable {
+
+}
